1196d1d6e0ad640f1b8554bb4d47d6e529c93b27
Anton Bobrov <bobrov@vl.ru>
2016-06-20 15:24:27 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dsq',
+    version='0.3',
+    url='https://github.com/baverman/dsq/',
+    license='MIT',
+    author='Anton Bobrov',
+    author_email='baverman@gmail.com',
+    description='Dead simple task queue',
+    long_description=open('README.md').read(),
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'redis >= 2.7.0',
+        'click >= 5.0.0',
+        'msgpack-python>=0.4.0',
+    ],
+    entry_points={
+        'console_scripts': ['dsq = dsq.cli:cli']
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet',
+        'Topic :: Scientific/Engineering',
+        'Topic :: System :: Distributed Computing',
+        'Topic :: System :: Systems Administration',
+        'Topic :: System :: Monitoring',
+    ]
+)
